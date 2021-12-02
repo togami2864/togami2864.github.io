17c79b216b36b43bd7c2c6ca70f723af00945f42
togami2864 <tuabobo123@gmail.com>
2021-12-02 18:51:06 +0900

    feat: add lang conponent

--- src/components/Lang.tsx
+++ src/components/Lang.tsx
@@ -1,18 +1,57 @@
 import React from 'react';
 import './Lang.css';
+import { Tooltip } from '@chakra-ui/react';
 
 interface LangProps {
-  isJaVer: boolean;
-  isLangAvailable?: boolean;
+  isJapanese: boolean;
+  isEnAvailable: boolean;
+  isJaAvailable: boolean;
 }
 
-export const Lang: React.FC<LangProps> = ({ isJaVer, isLangAvailable }) => {
+export const Lang: React.FC<LangProps> = ({
+  isJapanese,
+  isEnAvailable,
+  isJaAvailable,
+}) => {
+  console.log(isEnAvailable);
   return (
     <>
       <div className="lang">
-        <p className={!isJaVer && 'current_lang'}>en</p>
-        <p> / </p>
-        <p className={isJaVer && 'current_lang'}>ja</p>
+        <Tooltip
+          label="Sorry😢 English version is unavailable"
+          isDisabled={isEnAvailable}
+        >
+          <a
+            href=""
+            className={
+              !isEnAvailable
+                ? 'lang_unavailable'
+                : !isJapanese
+                ? 'current_lang'
+                : null
+            }
+          >
+            en
+          </a>
+        </Tooltip>
+        <p>/</p>
+        <Tooltip
+          label="Sorry😢 Japanese version is unavailable"
+          isDisabled={isJaAvailable}
+        >
+          <a
+            href=""
+            className={
+              !isJaAvailable
+                ? 'lang_unavailable'
+                : isJapanese
+                ? 'current_lang'
+                : null
+            }
+          >
+            ja
+          </a>
+        </Tooltip>
       </div>
     </>
   );
