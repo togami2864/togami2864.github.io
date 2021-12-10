8458feec1748103b340ad576605cb128e99a33dd
togami2864 <tuabobo123@gmail.com>
2021-12-10 09:15:58 +0000

    Deploy https://github.com/togami2864/togami2864.github.io/commit/79882fc23d29f382e2efead29717239da846b3ae

--- src/utils/getHeroImage.ts
+++
@@ -1,6 +0,0 @@
-const NUMBER_OF_HERO_IMAGES = 10;
-
-export const getHeroImage = () => {
-  const imageNumber = Math.floor(Math.random() * NUMBER_OF_HERO_IMAGES) + 1;
-  return `/hero/hero_${imageNumber}.jpeg`;
-};
