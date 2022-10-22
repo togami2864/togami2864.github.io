7807409be78a52f47df07f0f4d8140603be64f60
togami2864 <tuabobo123@gmail.com>
2022-10-22 21:11:03 +0900

    upgrade astro v0.25 -> v0.26

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [react()],
-  buildOptions: {
-    site: 'https://togami2864.github.io/',
-  },
+  base: 'https://togami2864.github.io/',
 });
