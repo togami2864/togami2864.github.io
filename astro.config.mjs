3239bb5033349d92d699da30382ab3d0b90adfc0
togami2864 <tuabobo123@gmail.com>
2022-10-24 22:45:17 +0900

    fix: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+// let base;
+// if (process.env.NODE_ENV !== 'production') {
+//   base = '';
+// } else {
+//   base = 'https://togami2864.github.io/';
+// }
 export default defineConfig({
   integrations: [react(), mdx()],
-  // base: 'https://togami2864.github.io/',
+  // base,
 });
