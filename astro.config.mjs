6c1b67218ca2e32f335b174b1905ae98ad2a184f
togami2864 <tuabobo123@gmail.com>
2022-11-10 21:54:42 +0900

    chore: remove dead code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
-// let base;
-// if (process.env.NODE_ENV !== 'production') {
-//   base = '';
-// } else {
-//   base = 'https://togami2864.github.io/';
-// }
 export default defineConfig({
   integrations: [react(), mdx()],
-  // base,
 });
